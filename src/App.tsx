e3181ff029b8b4db29512d2c9bd6a5f57e937196
HARIVOLA Andy <hei.andy.8@gmail.com>
2022-09-15 19:04:32 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Connexion from "./components/login/connexion";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
         <Connexion/>
+        <Navbar/>
     </>
   );
 }
